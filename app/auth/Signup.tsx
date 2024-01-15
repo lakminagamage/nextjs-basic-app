4a91c43f950acbc5f0937e601b5b6c24df562984
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2024-01-15 22:51:50 +0530

    Auth model | added linking to signup

--- app/auth/Signup.tsx
+++ app/auth/Signup.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
-const Signin = () => {
+
+const Signup = () => {
     return (
        <div className="w-full h-[600px] bg-black rounded-[50px] flex">
             <div className="flex mx-auto my-auto w-2/5 border-2 rounded-lg border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
@@ -12,14 +12,14 @@ const Signin = () => {
 
             <div className="flex flex-col w-2/5">
                 <div className="flex flex-col mx-20 my-20">
-                <p className='text-3xl my-10 text-center'>Sign In</p>
+                <p className='text-3xl my-10 text-center'>Sign Up</p>
                     <label htmlFor="email" className='text-xl'>Email</label>
                     <input type="email" placeholder='user@hypercube.com' name="email" id="email" className='border-2 border-black rounded-md p-2'/>
                     <label htmlFor="password" className='text-xl'>Password</label>
                     <input type="password" name="password" placeholder='Password' id="password" className='border-2 border-black rounded-md p-2'/>
-                    <button className='bg-teal text-white rounded-md p-2 mt-5'>Sign In</button>
-
-                    <p className="text-center my-5">Don't have an account? <Link href='/about'>Sign up.</Link></p>
+                    <label htmlFor="password" className='text-xl'>Confirm Password</label>
+                    <input type="password" name="password" placeholder='Password' id="password" className='border-2 border-black rounded-md p-2'/>
+                    <button className='bg-teal text-white rounded-md p-2 mt-5'>Sign Up</button>
                 </div>
              </div>    
             
@@ -27,4 +27,4 @@ const Signin = () => {
     );
     }
 
-export default Signin;
+export default Signup;
