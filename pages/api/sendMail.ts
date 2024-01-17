923ca64773c7d9c4577abe0af4f3d5261ccf73fa
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2024-01-17 22:30:04 +0530

    mail system | added uom smtp

--- pages/api/sendMail.ts
+++ pages/api/sendMail.ts
@@ -1,10 +1,24 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import nodemailer from 'nodemailer'
+import type { NextResponse } from 'next/server'
 
 
-export async function POST(req, res){
-  try{
-    const { name, email, message } = await req.body;
-    
-  }
-}
+// export async function POST(req : any, res : any){
+//   try{
+//     const { name, email, message } = await req.json;
+//     const transporter = nodemailer.createTransport({
+//       host: process.env.SMTP_HOST,
+//       port: process.env.SMTP_PORT,
+//       secure: false,
+//       auth:{
+//         user: process.env.SMTP_USER,
+//         pass: process.env.SMTP_PASS
+//       },
+  
+//     });
+
+//     const mailOptions = {
+//       from: process.env.SMTP_FROM,
+//       to: process.env.SMTP_TO,
+//       subject: `New message from ${name}`,
+//       text: message,
+//       replyTo: email
+//     }
